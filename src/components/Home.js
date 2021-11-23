6cd6c123f7192ffea9b2058de3fbd02bd9e2edcd
Dancanmilgo73 <dancanmilgo73@gmail.com>
2021-11-23 11:23:49 +0300

    Add links to login and register from homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import {Link } from "react-router-dom";
 export default function Home() {
     return (
         <div>
             <h1>Welcome Home</h1>
+            <p><Link to="/register">Register</Link> or <Link to="/login">Login</Link> if you already have an </p>
         </div>
     )
 }
